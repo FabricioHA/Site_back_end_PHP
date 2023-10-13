2c6f0f688d9f0dc43fb3e5db130517c487ee3d88
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2023-10-13 11:39:41 -0300

    Delete validarSessao.php

--- validarSessao.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if(!isset($_SESSION['logado']) && !isset($_SESSION['logado']) == 'logado')
-{
-    session_destroy();
-    header("location: http://localhost/Site_Software_Reconhecimento_De_Gestos/loginAdmin.html");
-}else;
-?>
