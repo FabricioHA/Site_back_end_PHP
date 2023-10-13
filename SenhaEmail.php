3953c1107f42fb42fcb4aff51bb9f35edb23a066
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2023-10-13 11:39:19 -0300

    Delete SenhaEmail.php

--- SenhaEmail.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('USER', 'softrecgestos@gmail.com');  // <-- Insira aqui do email que vai responder o usuário
-define('PWD', 'lu12fa06ed08vi27');    // <-- Insira aqui a senha do email que vai responder o usuário
-?>
